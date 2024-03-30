be7b1d8cceca9e1a4ed0f252ff79438e98b9385d
cvargas306 <cavbcr@gmail.com>
2024-03-30 01:21:37 -0600

    Task 05 complete

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,11 +1,22 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
-let templesElement;
+const templesElement = document.getElementById('temples');
 let templeList = [];
 
 /* async displayTemples Function */
 const displayTemples = async (temples) => {
+    temples.forEach(temple => {
+        let article = document.createElement('article');
+        let h3 = document.createElement('h3');
+        h3.textContent = temple.templeName;
+        let img = document.createElement('img');
+        img.src = temple.imageUrl;
+        img.alt = temple.location;
+        article.appendChild(h3);
+        article.appendChild(img);
+        templesElement.appendChild(article);
+    });
 
 };
 
@@ -35,6 +46,23 @@ const reset = () => {
 
 /* filterTemples Function */
 const filterTemples = (temples) => {
+    reset();
+    let filter = document.getElementById('filtered').value;
+    switch (filter) {
+        case 'utah':
+            displayTemples(temples.filter(temple => temple.location.includes('Utah')));
+            break;
+        case 'notutah':
+            displayTemples(temples.filter(temple => !temple.location.includes('Utah')));
+            break;
+        case 'older':
+            displayTemples(temples.filter(temple => new Date(temple.dedicated) < new Date(1950, 0, 1)));
+            break;
+        case 'all':
+        default:
+            displayTemples(temples);
+            break;
+    }
 
 };
 
@@ -45,3 +73,5 @@ getTemples();
 document.querySelector("#filtered").addEventListener("change", () => {
     filterTemples(templeList);
 });
+
+getTemples();
